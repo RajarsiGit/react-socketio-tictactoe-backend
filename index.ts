3330588f213c2f1b2a01016f5a816f356c23de97
rajarsi3997 <rajarsi3997@gmail.com>
2021-05-20 22:35:40 +0530

    Added Room Capabilities

--- index.ts
+++ index.ts
@@ -5,8 +5,14 @@ import socketio from 'socket.io';
 
 const app = express();
 app.use(cors());
-const PORT = process.env.PORT || 5000;
 
+const PORT: string | number = process.env.PORT || 5000;
+const room: { room: string, id: string[] }[] = new Array<{room: string, id: string[]}>();
+let i: number = 0;
+room[i] = {
+    room: '',
+    id: []
+};
 const httpServer = new http.Server(app)
 const io = new socketio.Server(httpServer, {
     cors: {
@@ -15,26 +21,43 @@ const io = new socketio.Server(httpServer, {
     }
 });
 
+const getRoom = (socket: {id: string}, room: { id: string[]; room: string; }[]): string[] => {
+    return room.map((room) => {
+        if (room.id[0] === socket.id || room.id[1] === socket.id) {
+            return room.room;
+        }
+    });
+}
+
 app.get('/', (_req, res) => {
     res.status(200).send('Javascript Template')
 });
 
 io.on('connection', socket => {
     console.log('User ' + socket.id + ' is trying to connect...')
-    if (io.sockets.adapter.sids.size > 2) {
-        console.log('Room Full!')
-        return;
+    if (room[i].id.length === 2) {
+        i++;
+        room.push({
+            room: 'room_' + i.toString(),
+            id: []
+        });
+        room[i].id.push(socket.id);
+        socket.join(room[i].room);
+        console.log('User ' + socket.id + ' is connected to room: ' + room[i].room);
+    } else {
+        room[i].room = 'room_' + i.toString();
+        room[i].id.push(socket.id);
+        socket.join('room_' + i.toString());
+        console.log('User ' + socket.id + ' is connected to room: ' + room[i].room);
     }
-    socket.join('prod');
-    console.log('User ' + socket.id + ' is connected');
     socket.on('sendChoice', (data) => {
-        socket.to('prod').emit('receiveChoice', data);
+        socket.to(getRoom(socket, room)).emit('receiveChoice', data);
     });
     socket.on('sendMatrix', (data) => {
-        socket.to('prod').emit('receiveMatrix', data);
+        socket.to(getRoom(socket, room)).emit('receiveMatrix', data);
     });
     socket.on('reload', (data) => {
-        socket.to('prod').emit('reload', data);
+        socket.to(getRoom(socket, room)).emit('reload', data);
     });
     socket.on('disconnect', () => {
        console.log('User ' + socket.id + ' is disconnected');
@@ -42,5 +65,5 @@ io.on('connection', socket => {
 });
 
 httpServer.listen(PORT, () => {
-    console.log('Listeningon PORT: ' + PORT);
+    console.log('Listening on PORT: ' + PORT);
 });
